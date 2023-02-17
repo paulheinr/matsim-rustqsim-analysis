edf1803cc491549b77e1d835273cd74d949a1907
Paul Heinrich <paul.heinrich@outlook.com>
2023-02-17 12:12:48 +0100

    Added per generic per hour evaluation

--- src/main/java/org/matsim/analysis/LinkAnalyzer.java
+++ src/main/java/org/matsim/analysis/LinkAnalyzer.java
@@ -9,7 +9,12 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LinkAnalyzer implements LinkEnterEventHandler, LinkLeaveEventHandler {
     private final List<LinkEnterEvent> linkEnterEventCache = new LinkedList<>();
@@ -44,20 +49,32 @@ public class LinkAnalyzer implements LinkEnterEventHandler, LinkLeaveEventHandle
         for (Id<Link> linkId : travelTimesByLinkId.keySet()) {
             System.out.println("-------------------------");
             System.out.println("Link: " + linkId.toString());
-            System.out.println("Travel times: " + travelTimesByLinkId.get(linkId));
-            System.out.println("Traffic flow: " + Arrays.toString(getTrafficFlowPerHour(linkId)));
+            System.out.println("Travel times: " + getMetricPerHour(linkId, this::getTravelTimeAverageBetween));
+            System.out.println("Traffic flow: " + getMetricPerHour(linkId, this::getTrafficFlowBetween));
         }
     }
 
-    private int[] getTrafficFlowPerHour(Id<Link> linkId) {
-        int[] result = new int[24];
-        for (int i = 0; i < 24; i++) {
-            int beginInSec = i * 60 * 60;
-            int endInSec = (i + 1) * 60 * 60;
-            result[i] = (int) travelTimesByLinkId.get(linkId).stream()
-                    .filter(info -> beginInSec <= info.timeOfLinkLeave && info.timeOfLinkLeave < endInSec).count();
-        }
-        return result;
+    private Integer getTrafficFlowBetween(Id<Link> linkId, Integer begin, Integer end) {
+        return Math.toIntExact(travelTimesByLinkId.get(linkId).stream()
+                .filter(info -> begin <= info.timeOfLinkLeave && info.timeOfLinkLeave < end).count());
+    }
+
+    private Double getTravelTimeAverageBetween(Id<Link> linkId, Integer begin, Integer end) {
+        return travelTimesByLinkId.get(linkId)
+                .stream()
+                .filter(info -> begin <= info.timeOfLinkLeave && info.timeOfLinkLeave < end)
+                .mapToDouble(info -> info.travelTime).average()
+                .orElse(0.);
+    }
+
+    private <R> List<R> getMetricPerHour(Id<Link> linkId, Function3<Id<Link>, Integer, Integer, R> getMetricBetweenTime) {
+        return IntStream.range(0, 24)
+                .mapToObj(hour -> {
+                    int beginInSec = hour * 60 * 60;
+                    int endInSec = (hour + 1) * 60 * 60;
+                    return getMetricBetweenTime.apply(linkId, beginInSec, endInSec);
+                })
+                .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
@@ -70,10 +87,15 @@ public class LinkAnalyzer implements LinkEnterEventHandler, LinkLeaveEventHandle
         handler.printResult();
     }
 
-    private record TravelTimeInformation(Double travelTime, Double timeOfLinkLeave) {
+    private record TravelTimeInformation(double travelTime, double timeOfLinkLeave) {
         @Override
         public String toString() {
-            return travelTime.toString();
+            return "{timeOfLeave: " + timeOfLinkLeave + " | travelTime: " + travelTime + "}";
         }
+    }
+
+    @FunctionalInterface
+    private interface Function3<A, B, C, R> {
+        public R apply(A a, B b, C c);
     }
 }
